7daf0f1cf3e34d148ddae4f1c923dd1a4f0d4b44
Cora Chan <corachan055@gmail.com>
2020-07-01 21:59:21 -0700

    Attempt 2

---
+++ precache-manifest.0bdebc79d22a71c9acc0a34e533952c3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f49edb90c26dbc57c3584fcd144760f8",
+    "url": "/0dc568d46e59e55786cc.worker.js"
+  },
+  {
+    "revision": "69cd844e5198ac97a0aad1207ced67af",
+    "url": "/0dc568d46e59e55786cc.worker.js.LICENSE.txt"
+  },
+  {
+    "revision": "c161b7bd22890a351cb46e7631e83c4c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "871e5a07583aab402b94",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "85d6772c3b8b1a5054b7",
+    "url": "/static/css/main.0e7c2695.chunk.css"
+  },
+  {
+    "revision": "871e5a07583aab402b94",
+    "url": "/static/js/2.64b240da.chunk.js"
+  },
+  {
+    "revision": "701364163004c819c2f7b712050a307d",
+    "url": "/static/js/2.64b240da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85d6772c3b8b1a5054b7",
+    "url": "/static/js/main.a4306172.chunk.js"
+  },
+  {
+    "revision": "216563cf50e7c35b2f13",
+    "url": "/static/js/runtime-main.7c0ae09a.js"
+  },
+  {
+    "revision": "f95347a51582ecb9fcd59bea260d94c6",
+    "url": "/static/media/Cora_Resume-1Jul2020.f95347a5.pdf"
+  },
+  {
+    "revision": "b2305fdba14845b61396a41c15d8d38f",
+    "url": "/static/media/Sunflowers.b2305fdb.JPG"
+  },
+  {
+    "revision": "09212fb62203b7e2f3d34379b646b033",
+    "url": "/static/media/SunflowersBlock.09212fb6.png"
+  },
+  {
+    "revision": "404ad411c76bba78b3227efcf15d2838",
+    "url": "/static/media/SunflowersCrop.404ad411.JPG"
+  }
+]);
