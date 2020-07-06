119f74b9791ad2097cc8f19a3ff2bae5be5603b0
Cora Chan <corachan055@gmail.com>
2020-07-05 22:12:48 -0700

    Minor changes

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fd2867c8f3fdcbadecbc6151c683ac60.js"
+  "/precache-manifest.8703bdcfcb2a105cf8000706fbfc8bbe.js"
 );
 
 self.addEventListener('message', (event) => {
