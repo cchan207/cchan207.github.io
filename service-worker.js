9b42f8c2c35830ba92bad7447c81f224d449362a
Cora Chan <corachan055@gmail.com>
2020-07-07 18:43:31 -0700

    Hashed based routing

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b85569cc30a3a6ea59d4dc542a2b8275.js"
+  "/precache-manifest.7210ed41985d2ce287426d7dbf443d9a.js"
 );
 
 self.addEventListener('message', (event) => {
